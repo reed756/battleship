f790222e496b08bce6cf02452bd1fd95466893d6
reed756 <68922323+reed756@users.noreply.github.com>
2021-07-06 16:11:41 +0100

    created function on gameboard to show if all ships have been sunk and also amended function to show that ships have been hit and got it to pass the test

--- src/__tests__/gameboard.test.js
+++ src/__tests__/gameboard.test.js
@@ -3,7 +3,6 @@ const ship = require('../ship');
 
 describe('arrayContaining', () => {
     const newGameBoard = gameboard();
-    const newShip = ship(5);
     const expected = [0,0,0,0,5,5,5,5,5,0,
         0,0,0,0,0,0,0,0,0,0,
         0,0,0,0,0,0,0,0,0,0,
@@ -14,7 +13,7 @@ describe('arrayContaining', () => {
         0,0,0,0,0,0,0,0,0,0,
         0,0,0,0,0,0,0,0,0,0,
         0,0,0,0,0,0,0,0,0,0]
-    newGameBoard.placeShipHorizontally(newGameBoard, newShip.length, 4)
+    newGameBoard.placeShipHorizontally(newGameBoard, newGameBoard.fleet.five.length, 4)
     it("Place one ship and matches where it's been put on the gameboard horizontally", () => {
         expect(newGameBoard.board).toEqual(expect.arrayContaining(expected));
     });
@@ -22,7 +21,6 @@ describe('arrayContaining', () => {
 
 describe('arrayContaining', () => {
     const newGameBoard = gameboard();
-    const newShip = ship(5);
     const expected = [0,0,0,0,5,0,0,0,0,0,
         0,0,0,0,5,0,0,0,0,0,
         0,0,0,0,5,0,0,0,0,0,
@@ -33,7 +31,7 @@ describe('arrayContaining', () => {
         0,0,0,0,0,0,0,0,0,0,
         0,0,0,0,0,0,0,0,0,0,
         0,0,0,0,0,0,0,0,0,0]
-    newGameBoard.placeShipVertically(newGameBoard, newShip.length, 4)
+    newGameBoard.placeShipVertically(newGameBoard, newGameBoard.fleet.five.length, 4)
     it("Place one ship and matches where it's been put on the gameboard vertically", () => {
         expect(newGameBoard.board).toEqual(expect.arrayContaining(expected));
     });
@@ -41,25 +39,20 @@ describe('arrayContaining', () => {
 
 describe('arrayContaining', () => {
     const newGameBoard = gameboard();
-    const oneShip = ship(1);
-    const twoShip = ship(2);
-    const threeShip = ship(3);
-    const fourShip = ship(4);
-    const fiveShip = ship(5);
-    newGameBoard.placeShipVertically(newGameBoard, oneShip.length, 4);
-    newGameBoard.placeShipHorizontally(newGameBoard, twoShip.length, 11);
-    newGameBoard.placeShipVertically(newGameBoard, threeShip.length, 55);
-    newGameBoard.placeShipHorizontally(newGameBoard, fourShip.length, 83);
-    newGameBoard.placeShipVertically(newGameBoard, fiveShip.length, 25);
+    newGameBoard.placeShipVertically(newGameBoard, newGameBoard.fleet.one.length, 4);
+    newGameBoard.placeShipHorizontally(newGameBoard, newGameBoard.fleet.two.length, 11);
+    newGameBoard.placeShipVertically(newGameBoard, newGameBoard.fleet.three.length, 54);
+    newGameBoard.placeShipHorizontally(newGameBoard, newGameBoard.fleet.four.length, 83);
+    newGameBoard.placeShipVertically(newGameBoard, newGameBoard.fleet.five.length, 25);
     const expected = [0,0,0,0,1,0,0,0,0,0,
         0,2,2,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,5,0,0,0,
-        0,0,0,0,0,0,5,0,0,0,
-        0,0,0,0,0,0,5,0,0,0,
-        0,0,0,0,0,0,5,0,0,0,
-        3,0,0,0,0,0,5,0,0,0,
-        3,0,0,0,0,0,0,0,0,0,
-        3,0,4,4,4,4,0,0,0,0,
+        0,0,0,0,0,5,0,0,0,0,
+        0,0,0,0,0,5,0,0,0,0,
+        0,0,0,0,0,5,0,0,0,0,
+        0,0,0,0,3,5,0,0,0,0,
+        0,0,0,0,3,5,0,0,0,0,
+        0,0,0,0,3,0,0,0,0,0,
+        0,0,0,4,4,4,4,0,0,0,
         0,0,0,0,0,0,0,0,0,0]
     it("Place multiple ships and matches where it's been put on the gameboard", () => {
         expect(newGameBoard.board).toEqual(expect.arrayContaining(expected));
@@ -68,28 +61,23 @@ describe('arrayContaining', () => {
 
 describe('arrayContaining', () => {
     const newGameBoard = gameboard();
-    const oneShip = ship(1);
-    const twoShip = ship(2);
-    const threeShip = ship(3);
-    const fourShip = ship(4);
-    const fiveShip = ship(5);
-    newGameBoard.placeShipVertically(newGameBoard, oneShip.length, 4);
-    newGameBoard.placeShipHorizontally(newGameBoard, twoShip.length, 11);
-    newGameBoard.placeShipVertically(newGameBoard, threeShip.length, 55);
-    newGameBoard.placeShipHorizontally(newGameBoard, fourShip.length, 83);
-    newGameBoard.placeShipVertically(newGameBoard, fiveShip.length, 25);
+    newGameBoard.placeShipVertically(newGameBoard, newGameBoard.fleet.one.length, 4);
+    newGameBoard.placeShipHorizontally(newGameBoard, newGameBoard.fleet.two.length, 11);
+    newGameBoard.placeShipVertically(newGameBoard, newGameBoard.fleet.three.length, 54);
+    newGameBoard.placeShipHorizontally(newGameBoard, newGameBoard.fleet.four.length, 83);
+    newGameBoard.placeShipVertically(newGameBoard, newGameBoard.fleet.five.length, 25);
     newGameBoard.receiveAttack(newGameBoard, 5);
     newGameBoard.receiveAttack(newGameBoard, 15);
-    newGameBoard.receiveAttack(newGameBoard, 45);
+    newGameBoard.receiveAttack(newGameBoard, 46);
     const expected = [0,0,0,0,1,-1,0,0,0,0,
         0,2,2,0,0,-1,0,0,0,0,
-        0,0,0,0,0,0,5,0,0,0,
-        0,0,0,0,0,0,5,0,0,0,
-        0,0,0,0,0,0,5,0,0,-1,
-        0,0,0,0,0,0,5,0,0,0,
-        3,0,0,0,0,0,5,0,0,0,
-        3,0,0,0,0,0,0,0,0,0,
-        3,0,4,4,4,4,0,0,0,0,
+        0,0,0,0,0,5,0,0,0,0,
+        0,0,0,0,0,5,0,0,0,0,
+        0,0,0,0,0,5,-1,0,0,0,
+        0,0,0,0,3,5,0,0,0,0,
+        0,0,0,0,3,5,0,0,0,0,
+        0,0,0,0,3,0,0,0,0,0,
+        0,0,0,4,4,4,4,0,0,0,
         0,0,0,0,0,0,0,0,0,0]
     it("Check that shots show as missed on gameboard", () => {
         expect(newGameBoard.board).toEqual(expect.arrayContaining(expected));
@@ -98,23 +86,70 @@ describe('arrayContaining', () => {
 
 test('Shows ship has been hit on ship object', () => {
     const newGameBoard = gameboard();
-    const fleet = {
-        one: ship(1),
-        two: ship(2),
-        three: ship(3),
-        four: ship(4),
-        five: ship(5)
-    };
-    newGameBoard.placeShipVertically(newGameBoard, fleet.one.length, 4);
-    newGameBoard.placeShipHorizontally(newGameBoard, fleet.two.length, 11);
-    newGameBoard.placeShipVertically(newGameBoard, fleet.three.length, 55);
-    newGameBoard.placeShipHorizontally(newGameBoard, fleet.four.length, 83);
-    newGameBoard.placeShipVertically(newGameBoard, fleet.five.length, 25);
-    newGameBoard.receiveAttack(newGameBoard, 11, fleet);
-    expect(fleet.two).toMatchObject({
+    newGameBoard.placeShipVertically(newGameBoard, newGameBoard.fleet.one.length, 4);
+    newGameBoard.placeShipHorizontally(newGameBoard, newGameBoard.fleet.two.length, 11);
+    newGameBoard.placeShipVertically(newGameBoard, newGameBoard.fleet.three.length, 54);
+    newGameBoard.placeShipHorizontally(newGameBoard, newGameBoard.fleet.four.length, 83);
+    newGameBoard.placeShipVertically(newGameBoard, newGameBoard.fleet.five.length, 25);
+    newGameBoard.receiveAttack(newGameBoard, 11);
+    expect(newGameBoard.fleet.two).toMatchObject({
         length: 2,
         hitWhere: [11],
         sunk: false
     });
 });
+
+test('Shows ship has been sunk on ship object', () => {
+    const newGameBoard = gameboard();
+    newGameBoard.placeShipVertically(newGameBoard, newGameBoard.fleet.one.length, 4);
+    newGameBoard.placeShipHorizontally(newGameBoard, newGameBoard.fleet.two.length, 11);
+    newGameBoard.placeShipVertically(newGameBoard, newGameBoard.fleet.three.length, 54);
+    newGameBoard.placeShipHorizontally(newGameBoard, newGameBoard.fleet.four.length, 83);
+    newGameBoard.placeShipVertically(newGameBoard, newGameBoard.fleet.five.length, 25);
+    newGameBoard.receiveAttack(newGameBoard, 11);
+    newGameBoard.receiveAttack(newGameBoard, 12);
+    newGameBoard.fleet.two.isSunk(newGameBoard.fleet.two);
+    expect(newGameBoard.fleet.two.sunk).toEqual(true);
+});
+
+test('Shows all ships have been sunk on gameboard object', () => {
+    const newGameBoard = gameboard();
+    newGameBoard.placeShipVertically(newGameBoard, newGameBoard.fleet.one.length, 4);
+    newGameBoard.placeShipHorizontally(newGameBoard, newGameBoard.fleet.two.length, 11);
+    newGameBoard.placeShipVertically(newGameBoard, newGameBoard.fleet.three.length, 54);
+    newGameBoard.placeShipHorizontally(newGameBoard, newGameBoard.fleet.four.length, 83);
+    newGameBoard.placeShipVertically(newGameBoard, newGameBoard.fleet.five.length, 25);
+    newGameBoard.receiveAttack(newGameBoard, 4);
+    newGameBoard.receiveAttack(newGameBoard, 11);
+    newGameBoard.receiveAttack(newGameBoard, 12);
+    newGameBoard.receiveAttack(newGameBoard, 54);
+    newGameBoard.receiveAttack(newGameBoard, 64);
+    newGameBoard.receiveAttack(newGameBoard, 74);
+    newGameBoard.receiveAttack(newGameBoard, 83);
+    newGameBoard.receiveAttack(newGameBoard, 84);
+    newGameBoard.receiveAttack(newGameBoard, 85);
+    newGameBoard.receiveAttack(newGameBoard, 86);
+    newGameBoard.receiveAttack(newGameBoard, 25);
+    newGameBoard.receiveAttack(newGameBoard, 35);
+    newGameBoard.receiveAttack(newGameBoard, 45);
+    newGameBoard.receiveAttack(newGameBoard, 55);
+    newGameBoard.receiveAttack(newGameBoard, 65);
+    expect(newGameBoard.allSunk(newGameBoard)).toEqual(true);
+});
+
+test('Shows all ships have not been sunk on gameboard object', () => {
+    const newGameBoard = gameboard();
+    newGameBoard.placeShipVertically(newGameBoard, newGameBoard.fleet.one.length, 4);
+    newGameBoard.placeShipHorizontally(newGameBoard, newGameBoard.fleet.two.length, 11);
+    newGameBoard.placeShipVertically(newGameBoard, newGameBoard.fleet.three.length, 54);
+    newGameBoard.placeShipHorizontally(newGameBoard, newGameBoard.fleet.four.length, 83);
+    newGameBoard.placeShipVertically(newGameBoard, newGameBoard.fleet.five.length, 25);
+    newGameBoard.receiveAttack(newGameBoard, 4);
+    newGameBoard.receiveAttack(newGameBoard, 11);
+    newGameBoard.receiveAttack(newGameBoard, 55);
+    newGameBoard.receiveAttack(newGameBoard, 75);    
+    expect(newGameBoard.allSunk(newGameBoard)).toEqual(false);
+});
+
+
 
--- src/gameboard.js
+++ src/gameboard.js
@@ -15,6 +15,14 @@ const gameboard = function() {
         0,0,0,0,0,0,0,0,0,0,
     ]
 
+    const fleet = {
+        one: ship(1),
+        two: ship(2),
+        three: ship(3),
+        four: ship(4),
+        five: ship(5)
+    }
+
     const placeShipHorizontally = function (game, shipLength, start) {
         for (let i = start; i <= (shipLength + start); i++) {
             game.board[i] += shipLength;
@@ -27,30 +35,55 @@ const gameboard = function() {
         }
     }
 
-    const receiveAttack = function (game, point, fleet) {
+    const receiveAttack = function (game, point) {
+
         if (game.board[point] === 0) {
             game.board[point] -= 1;
-        } else if (game.board[point] > 0) {
-            switch(game.board[point]) {
-                case 1:
-                    fleet.one.hit(point);
-                  break;
-                case 2:
-                    fleet.two.hit(point);
-                  break;
-                case 3:
-                    fleet.three.hit(point);
-                  break;
-                case 4:
-                    fleet.four.hit(point);
-                  break;
-                case 5:
-                    fleet.five.hit(point);
-                  break;
-              }
+        } 
+        
+        if (game.board[point] > 0) {
+            if (game.board[point] === 1) {
+                game.fleet.one.hit(point);
+                return game.fleet.one;
+            } else if (game.board[point] === 2) {
+                game.fleet.two.hit(point);
+                return game.fleet.two;
+            } else if (game.board[point] === 3) {
+                game.fleet.three.hit(point);
+                return game.fleet.three;
+            } else if (game.board[point] === 4) {
+                game.fleet.four.hit(point);
+                return game.fleet.four;
+            } else if (game.board[point] === 5) {
+                game.fleet.five.hit(point);
+                return game.fleet.five;
+            }
+        }
+
+    }
+
+    const allSunk = function (game) {
+        game.fleet.one.isSunk(game.fleet.one);
+        game.fleet.two.isSunk(game.fleet.two);
+        game.fleet.three.isSunk(game.fleet.three);
+        game.fleet.four.isSunk(game.fleet.four);
+        game.fleet.five.isSunk(game.fleet.five);
+
+        if (game.fleet.one.sunk === false) {
+            return false;
+        } else if (game.fleet.two.sunk === false) {
+            return false;
+        } else if (game.fleet.three.sunk === false) {
+            return false;
+        } else if (game.fleet.four.sunk === false) {
+            return false;
+        } else if (game.fleet.five.sunk === false) {
+            return false;
+        } else { 
+            return true;
         }
     }
 
-    return { board, placeShipHorizontally, placeShipVertically, receiveAttack };
+    return { board, fleet, placeShipHorizontally, placeShipVertically, receiveAttack, allSunk };
 }
 module.exports = gameboard;
